0fcca358544ae15494a0c4c13ba98d14ca26ba6d
theshriramgupta <71961083+theshriramgupta@users.noreply.github.com>
2023-01-26 13:42:58 +0530

    Create binary-tree-inorder-transversal-leetcode-using-stack.cpp

---
+++ binary-tree-inorder-transversal-leetcode-using-stack.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root){
+        vector<int> ans;
+        stack<TreeNode*>S;
+        if(root != NULL)S.push(root);
+        while(!S.empty()){
+            TreeNode* temp = S.top();
+            if(temp ->left != NULL){
+                S.push(temp ->left);
+                temp ->left = NULL;
+            }
+            else{
+                ans.push_back(temp ->val);
+                S.pop();
+                if(temp ->right != NULL){
+                    S.push(temp ->right);
+                }
+            }
+        }
+        return ans;
+    }
+};
