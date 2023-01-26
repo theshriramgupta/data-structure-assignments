d8887940d22656fea8df7e56d0d172bc10d40bc7
theshriramgupta <71961083+theshriramgupta@users.noreply.github.com>
2023-01-26 15:28:12 +0530

    Create binary-preorder-tree-using-stack-leetcode.cpp

---
+++ binary-preorder-tree-using-stack-leetcode.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+
+        stack<TreeNode*>S;
+        if(root)S.push(root);
+        while(!S.empty()){
+            TreeNode* temp = S.top();
+            ans.push_back(temp ->val);
+            S.pop();
+
+            if(temp ->left){
+                S.push(temp ->left);
+                temp ->left = NULL;
+            }
+            else{
+                if(temp ->right){
+                    S.push(temp ->right);
+                    temp ->right = NULL;
+                }
+            }
+        }
+        return ans;
+    }
+};
